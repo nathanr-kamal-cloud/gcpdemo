d6ba0d42f7bfa4644de6487b856d822e80aff9c1
Kamalanathan Ranganathan <137532634+nathanr-kamal-cloud@users.noreply.github.com>
2023-06-24 06:39:03 +0530

    Initial commit

---
+++ src/main/java/com/cloud/gcpdemo/GcpdemoApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.gcpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpdemoApplication.class, args);
+	}
+
+}
